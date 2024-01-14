1ed2c400dba308327db4e39f77aed43c7649eb9a
Paul Laborderie <plaborderie@pm.me>
2024-01-14 05:08:30 +0100

    class -> className

--- app/routes/posts.$postId.jsx
+++ app/routes/posts.$postId.jsx
@@ -14,7 +14,7 @@ export default function Post() {
     const post = useLoaderData();
     return (
         <>
-            <h1 class="text-xl mb-4">{post.title} By {post.author}</h1>
+            <h1 className="text-xl mb-4">{post.title} By {post.author}</h1>
             <p className="mb-4 whitespace-pre-line">{post.content}</p>
             <div className="flex flex-row"><ChevronUpIcon className="h-6 w-6 mr-2" /> {post.score} <ChevronDownIcon className="h-6 w-6 ml-2" /></div>
         </>
