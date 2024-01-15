17911e2c81febd5ce925a9324b657757ff6f33a0
Paul Laborderie <plaborderie@pm.me>
2024-01-15 13:41:31 +0100

    Adding basic authentication

--- app/components/PostList.jsx
+++ app/components/PostList.jsx
@@ -1,4 +1,4 @@
-// import { ChatBubbleLeftIcon, CheckCircleIcon } from '@heroicons/react/24/outline'
+import { Link } from '@remix-run/react';
 
 export default function PostList({ posts }) {
   return (
@@ -10,9 +10,9 @@ export default function PostList({ posts }) {
         >
           <div>
             <p className="text-sm font-semibold leading-6 text-gray-900">
-              <a href={`/posts/${post.id}`} className="hover:underline">
+              <Link to={`/posts/${post.id}`} className="hover:underline">
                 {post.title}
-              </a>
+              </Link>
             </p>
             <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
               <p>
@@ -26,31 +26,6 @@ export default function PostList({ posts }) {
               </p>
             </div>
           </div>
-          {/* <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
-            <div className="flex -space-x-0.5">
-              <dt className="sr-only">Commenters</dt>
-              {post.commenters.map((commenter) => (
-                <dd key={commenter.id}>
-                  <img
-                    className="h-6 w-6 rounded-full bg-gray-50 ring-2 ring-white"
-                    src={commenter.imageUrl}
-                    alt={commenter.name}
-                  />
-                </dd>
-              ))}
-            </div>
-            <div className="flex w-16 gap-x-2.5">
-              <dt>
-                <span className="sr-only">Total comments</span>
-                {post.status === 'resolved' ? (
-                  <CheckCircleIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-                ) : (
-                  <ChatBubbleLeftIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
-                )}
-              </dt>
-              <dd className="text-sm leading-6 text-gray-900">{post.totalComments}</dd>
-            </div>
-          </dl> */}
         </li>
       ))}
     </ul>
--- app/root.jsx
+++ app/root.jsx
@@ -1,40 +1,77 @@
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  json,
+  useLoaderData,
+  useRevalidator,
+} from "@remix-run/react";
+import { useEffect, useState } from 'react';
+import { createBrowserClient, createServerClient } from '@supabase/auth-helpers-remix';
 
-  import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
+import stylesheet from "./tailwind.css";
 
-  import stylesheet from "./tailwind.css";
+export const links = () => [
+{ rel: 'stylesheet', href: stylesheet }
+];
 
-  export const links = () => [
-    { rel: 'stylesheet', href: stylesheet }
-  ];
-  
-  export default function App() {
-    return (
-      <html>
-        <head>
-          <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Navbar />
-          <div className="container mx-auto">
-            <Outlet />
-          </div>
-  
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    );
+export const loader = async ({ request }) => {
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   }
-  
+
+  const response = new Response();
+
+  const supabase = createServerClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, {
+    request,
+    response,
+  });
+
+  const { data: { session } } = await supabase.auth.getSession();
+
+  return json({ env, session }, { headers: response.headers });
+}
+
+export default function App() {
+  const { env, session } = useLoaderData();
+  const { revalidate } = useRevalidator();
+
+  const [supabase] = useState(() => createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY));
+
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        revalidate(); // server and client are out of sync
+      }
+    });
+
+    return () => { subscription.unsubscribe() };
+  }, [serverAccessToken, supabase, revalidate]);
+
+  return (
+    <html className="min-h-full bg-white">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/x-icon;base64,AA"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body className="min-h-full">
+        <Navbar />
+        <div className="container mx-auto">
+          <Outlet context={{ supabase }} />
+        </div>
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
