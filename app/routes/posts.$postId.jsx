2e4759cae0070dcb060b83f283610c1d8dc0ee79
Paul Laborderie <plaborderie@pm.me>
2024-01-16 06:37:57 +0100

    Working on votes

--- app/routes/posts.$postId.jsx
+++ app/routes/posts.$postId.jsx
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node";
 import {
     useLoaderData,
+    useOutletContext,
   } from "@remix-run/react";
   import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
 
@@ -12,11 +13,49 @@ export const loader = async ({ params }) => {
 
 export default function Post() {
     const post = useLoaderData();
+    const { supabase } = useOutletContext();
+
+    const vote = async (value) => {
+        const { data: { session } } = await supabase.auth.getSession();
+        const jwt = session?.access_token;
+        await fetch(`https://localhost:3000/user-votes/${post.id}/vote/${value}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwt 
+            }
+        });
+    };
+
+    const upvote = async () => {
+        await vote(1);
+    };
+
+    const downvote = async () => {
+        await vote(-1);
+    };
+
+    const cancelVote = async () => {
+        const { data: { session } } = await supabase.auth.getSession();
+        const jwt = session?.access_token;
+        await fetch(`https://localhost:3000/user-votes/${post.id}/vote`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwt 
+            }
+        });
+    }
+
     return (
         <>
             <h1 className="text-xl mb-4">{post.title} By {post.author}</h1>
             <p className="mb-4 whitespace-pre-line">{post.content}</p>
-            <div className="flex flex-row"><ChevronUpIcon className="h-6 w-6 mr-2" /> {post.score} <ChevronDownIcon className="h-6 w-6 ml-2" /></div>
+            <div className="flex flex-row">
+                <ChevronUpIcon className="h-6 w-6 mr-2" onClick={upvote} />
+                {post.score}
+                <ChevronDownIcon className="h-6 w-6 ml-2" onClick={downvote} />
+            </div>
         </>
     )
 }
